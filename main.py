4ca1732d873037f196333ba87950407a302e6350
tassaron2 <rutachigal@gmail.com>
2020-10-10 16:34:39 -0400

    reformat with black + add batch file game to projects

--- main.py
+++ main.py
@@ -3,29 +3,37 @@ Entrypoint to flask application for tassaron's portfolio website
 """
 import flask
 
-#import os
 
 app = flask.Flask(__name__)
-#app.secret_key = os.urandom(16)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return flask.render_template('index.html')
+    return flask.render_template("index.html")
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return flask.render_template('about.html')
+    return flask.render_template("about.html")
 
-@app.route('/projects/')
+
+@app.route("/projects/")
 def projectsBaseDir():
     return flask.redirect("https://rainey.tech", code=301)
 
-@app.route('/projects/<projectName>')
+
+@app.route("/projects/<projectName>")
 def projects(projectName):
-    if projectName == 'pyaudiovis':
-        return flask.redirect("https://github.com/djfun/audio-visualizer-python/tree/feature-newgui", code=301)
-    elif projectName == 'breakout':
+    if projectName == "pyaudiovis":
+        return flask.redirect(
+            "https://github.com/djfun/audio-visualizer-python/tree/feature-newgui",
+            code=301,
+        )
+    elif projectName == "breakout":
         return flask.redirect("https://fun.tassaron.com/breakout", code=301)
+    elif projectName == "funtimes":
+        return flask.redirect("http://bat.tassaron.com", code=301)
+
 
 """
 @app.route('/newpost', methods=['POST'])
