936e5e8738b33f31fc12b436897635d2bc776a15
tassaron2 <rutachigal@gmail.com>
2020-12-11 19:17:45 -0500

    add flashed message back in & append sender email to message

--- contact.py
+++ contact.py
@@ -26,19 +26,16 @@ contact_blueprint = Blueprint("contact", __name__)
 @contact_blueprint.route("/contact", methods=["GET", "POST"])
 def contact():
     form = ContactForm()
-    if form.validate_on_submit():
-        if os.environ["FLASK_ENV"] == "development":
-            flash("Email sent! Thanks", "success")
-            return render_template("contact.html", form=form)
-        # Production
+    if os.environ["FLASK_ENV"] == "production" and form.validate_on_submit():
         try:
-            send_email(form.mail_subject.data, form.mail_body.data)
+            send_email(form.mail_subject.data, form.mail_body.data, form.sent_by.data)
+            flash("Email sent! Thanks", "success")
         except:
             flash("There was an error sending the email", "warning")
     return render_template("contact.html", form=form)
 
 
-def send_email(subject, body):
+def send_email(subject, body, respond_to):
     return requests.post(
         f"{EMAIL_API_URL}/messages",
         auth=("api", EMAIL_API_KEY),
@@ -46,6 +43,6 @@ def send_email(subject, body):
             "from": f"{EMAIL_SENDER_NAME} <{EMAIL_SENDER_ADDRESS}>",
             "to": [f"{EMAIL_RECEIVER_ADDRESS}"],
             "subject": subject,
-            "text": body,
+            "text": f"{body}\n\nRESPOND TO: {respond_to}",
         },
     )
